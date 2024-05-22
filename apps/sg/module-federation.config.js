7c6245c289e59838d2a42372e508b17607a3eeb7
kalingaCoder <rajesh.ad45@gmail.com>
2024-05-22 09:49:02 +0530

    Code cleanup

--- apps/sg/module-federation.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'sg',
-  exposes: {
-    './Module': './src/remote-entry.ts',
-  }
-};
--- packages/server/src/routes/routes-container.ts
+++
@@ -1,7 +0,0 @@
-import { Express } from "express";
-
-export function addRoutes(app: Express): void {
-  app.get("/", (req, res) => {
-    res.send("Hello, world!");
-  });
-}
